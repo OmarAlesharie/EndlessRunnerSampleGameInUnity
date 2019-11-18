07c5d85ab8f9099d33472c85a59df046b1d0f2c5
Omar Alesharie <omar.alesharie@gmail.com>
2019-11-18 21:22:12 +0300

    Foxing Prefab problem
    InCorrect Pivot

---
+++ EndlessRunnerSampleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void StopJumping()
+    {
+        animator.SetBool("isJumping", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+    }
+}
