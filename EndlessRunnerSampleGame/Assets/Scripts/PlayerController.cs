3b5e440cf467df968dad23b7aaf7919ce385d657
Omar Alesharie <omar.alesharie@gmail.com>
2019-11-24 01:51:26 +0300

    Remove DisableChangeDirectionTrigger.cs

--- EndlessRunnerSampleGame/Assets/Scripts/PlayerController.cs
+++ EndlessRunnerSampleGame/Assets/Scripts/PlayerController.cs
@@ -89,8 +89,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Can Change Direction: " + canChangeDirection.ToString());
-
         if (isDead)
         {
             return;
